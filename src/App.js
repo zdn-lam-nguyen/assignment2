55a0d04cf5d034583434fa0646592bf6a03a53ed
Duong Pham <duong891109@gmail.com>
2023-02-21 14:26:21 +0700

    Pha hoai n+2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AddUser from "./components/AddUser";
 import UserList from "./components/UserList";
 
 function App() {
-  return <div className="App">{ <UserList /> }</div>;
+  return <div className="App">{<AddUser />}</div>;
 }
 
 export default App;
